3c4d6eea0f6d67220d2b661ac9f3806048072f17
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-19 18:28:30 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'site-nuxt',
+    title: 'GrahamSH',
     htmlAttrs: {
       lang: 'en'
     },
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.svg' }
     ]
   },
   tailwindcss: {
