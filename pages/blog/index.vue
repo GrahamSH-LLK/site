09f442fb26b465ca06a08e6a243692063ccdfec7
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-25 11:12:28 -0500

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="container mx-auto px-4 mt-28 md:mt-16 ">
+  <main class="container mx-auto px-4 mt-28 md:mt-20 ">
     <h1
       class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
     >
