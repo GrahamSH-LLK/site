5788f9e84f4ca22ff8e81115203f78beb02bad1a
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-25 11:12:09 -0500

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
 <main class="container mx-auto px-4 mt-28 md:mt-20">
 <div class="prose">
 <h2 id="wave-hi-im-grahamsh-llk">
-<img class="emoji" title=":wave:" alt=":wave:" src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png" height="20" width="20"> Hi, I’m GrahamSH-LLK</h2>
+<img class="emoji inline-block" title=":wave:" alt=":wave:" src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png" height="20" width="20"> Hi, I’m GrahamSH-LLK</h2>
 <p>I’m GrahamSH-LLK, Scratcher, Coder, and tech lover. There’s not much here right now, but more will be coming soon!</p>
 </div>
 </main>
