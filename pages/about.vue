ef01f4044501246719127c41dd28ba2418603146
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-26 12:29:23 -0500

    Start contact page

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,27 @@
 <template>
-<main class="container mx-auto px-4 mt-28 md:mt-20 h-screen">
-<div class="prose">
-<h2 id="wave-hi-im-grahamsh-llk">
-<img class="emoji inline-block" title=":wave:" alt=":wave:" src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png" height="20" width="20"> Hi, I’m GrahamSH-LLK</h2>
-<p>I’m GrahamSH-LLK, Scratcher, Coder, and tech lover. There’s not much here right now, but more will be coming soon!</p>
-</div>
-</main>
+  <main class="container mx-auto px-4 mt-28 md:mt-20 h-screen">
+    <h1
+      class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+    >
+      About
+    </h1>
+
+    <div class="prose">
+      <h2 id="wave-hi-im-grahamsh-llk">
+        <img
+          class="emoji inline-block"
+          title=":wave:"
+          alt=":wave:"
+          src="https://github.githubassets.com/images/icons/emoji/unicode/1f44b.png"
+          height="20"
+          width="20"
+        />
+        Hi, I’m GrahamSH-LLK
+      </h2>
+      <p>
+        I’m GrahamSH-LLK, Scratcher, Coder, and tech lover. There’s not much
+        here right now, but more will be coming soon!
+      </p>
+    </div>
+  </main>
 </template>
---
+++ pages/contact.vue
@@ -0,0 +1,63 @@
+<template>
+  <main class="container mx-auto px-4 mt-28 md:mt-20 h-screen">
+    <h1
+      class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+    >
+      Contact
+    </h1>
+
+    <form class="w-full max-w-lg">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            Username
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="GrahamSH"
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            Message
+          </label>
+          <textarea
+            class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            id="message"
+          ></textarea>
+          <p class="text-gray-600 text-xs italic">
+            What do you want to tell me? Write above.
+          </p>
+        </div>
+      </div>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3">
+          <button
+            class="shadow bg-indigo-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >
+            Send
+          </button>
+        </div>
+        <div class="md:w-2/3"></div>
+      </div>
+    </form>
+  </main>
+</template>
+<script>
+export default {
+  methods: {
+    send() {}
+  }
+};
+</script>
