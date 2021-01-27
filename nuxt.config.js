e8a9d2446947da33a390269a3b99fc3f66ba2577
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-27 16:14:38 -0500

    Changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,16 @@ export default {
   },
   tailwindcss: {
     config: {
-      plugins: [tailwindTypography]
+      plugins: [tailwindTypography],
+      theme: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+        }
+       },
+    
     }
   },
 
