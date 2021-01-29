4f93f5b18da5fbed99efa220205c4c4f112798e8
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-29 13:20:35 -0500

    Create NextPrev.vue

---
+++ components/global/NextPrev.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NuxtLink v-if="prev" :to="{ name: 'blog-slug', params: { slug: prev.slug } }">
+      {{ prev.title }}
+    </NuxtLink>
+
+    <NuxtLink v-if="next" :to="{ name: 'blog-slug', params: { slug: next.slug } }">
+      {{ next.title }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const [prev, next] = await $content('articles')
+      .only(['title', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .surround(params.slug)
+      .fetch()
+
+    return {
+      prev,
+      next
+    }
+  }
+}
+</script>
