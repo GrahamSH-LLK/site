c2acceedc4a08ae96575f181da246f0440a12463
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-02-10 18:17:21 -0500

    Update Search.vue

--- components/Search.vue
+++ components/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-         <div class="md:block mx-auto text-gray-600 hidden">
+         <div class="md:block lg:block mx-auto text-gray-600 hidden">
 
         <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
 
