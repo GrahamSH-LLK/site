0396aca5c35ce3929b6941592a06c3d9ae7e1910
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-02-24 15:53:25 +0000

    deploy: 03f1adfc527dd0b9d7c2b10de85c170f94a1c033

--- _nuxt/c69e37b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{295:function(t,e,n){"use strict";n.r(e);var l=n(11),component=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"bg-blue-500 text-white p-4 mb-4"},[e("p",[this._t("info-box")],2)])}),[],!1,null,null,null);e.default=component.exports}}]);
