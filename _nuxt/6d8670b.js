da5cf81523ac5d8226be5e816370b358f2369783
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-06-02 20:37:39 +0000

    deploy: 7d04195ea18208a5e95df43a0a0105ff12e41d42

---
+++ _nuxt/6d8670b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{294:function(t,e,n){"use strict";n.r(e);var l=n(12),component=Object(l.a)({},(function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"bg-indigo-500 rounded text-white p-4 mb-4"},[n("p",[t._t("default")],2)])}),[],!1,null,null,null);e.default=component.exports}}]);
